53fd75b96d7a40f8b5cf8f1cd3defe6af468edaf
fishcg <353740902@qq.com>
2018-10-11 10:25:39 +0800

    新增优先压缩音频接口 (#8)
    * 新增优先压缩音频接口
    
    * 规范新增压缩任务的代码格式

--- component/CandidateList.js
+++ component/CandidateList.js
@@ -1,5 +1,6 @@
 const R = require("ramda");
 const { getDB } = require("../lib/DBConnection");
+const { findIndexByAttr } = require("../lib/Utils");
 
 const candidates = [];
 
@@ -11,17 +12,27 @@ async function getSounds() {
     return rows;
 }
 
+/**
+ * 根据 ID 获取音频相关信息
+ *
+ * @param {Number} id 音频 ID
+ * @return {Object} 音频信息组成的对象
+ * @throws Error 连接数据库失败时抛出异常
+ */
+async function getSound(id) {
+  let conn = await getDB();
+  let sql = `SELECT id, user_id, duration, soundurl, checked FROM app_missevan.m_sound WHERE id = ${id}`
+  let row = await conn.findOne(sql);
+  return row;
+}
+
 candidates.__proto__.update = async function() {
     let rows = await getSounds();
     let time = Math.floor(Date.now() / 1000);
-    new_elem:
     for (let row of rows) {
-        for (let sound of this) {
-            if (row.id === sound.id) continue new_elem;
-        }
         row.time = time;
         row.run = false;
-        this.push(row);
+        addTask(row, false);
     }
     // @TODO 这里需要对任务优先级进行处理
 }
@@ -39,17 +50,61 @@ candidates.__proto__.getTasks = function(num) {
     return tasks;
 }
 
+/**
+ * 新增压缩任务
+ *
+ * @param {Sound} sound 音频对象
+ * @param {Boolean} isTop 是否添加到队列头
+ */
+function addTask(sound, isTop){
+    let index = findIndexByAttr('id', sound.id, candidates)
+    if (index > 0) {
+      if (candidates[index].run || !isTop) {
+        // 当音频已压缩或不需要置顶时，不做改动
+        return
+      }
+      sound = candidates[index]
+      // 删除原来的元素再进行插入，避免删除前就被取出的情况
+      candidates.splice(index, 1)
+      candidates.unshift(sound)
+    } else {
+      // 当音频不在压缩数组队列中时，将其加入队列
+      sound.time = Math.floor(Date.now() / 1000)
+      sound.run = false
+      if (isTop) {
+        candidates.unshift(sound)
+      } else {
+        candidates.push(sound);
+      }
+    }
+}
+
 candidates.__proto__.finishTask = function(id) {
-    let index = R.findIndex(R.propEq("id", id))(this);
+    let index = findIndexByAttr('id', id, this)
     if (-1 === index) return false;
     this.splice(index, 1);
     return true;
 }
 
-module.exports = candidates;
+/**
+ * 置顶指定的音频
+ *
+ * @param {Number} id 音频 ID
+ * @return {boolean} 是否置顶成功
+ * @throws 音频不存在或已转码时抛出异常
+ */
+candidates.__proto__.topSound = async function (id) {
+    let sound = await getSound(id)
+    if (!sound) {
+        throw new Error('该音频不存在或已转码')
+    }
+    // 将音频加入压缩任务队列头
+    addTask(sound, true)
+}
 
+module.exports = candidates;
 // @WORKAROUND 监控代码
 setInterval(() => {
     console.log(candidates);
     console.log("\n\n");
-}, 5000);
+}, 5000)
--- controller/TaskController.js
+++ controller/TaskController.js
@@ -1,5 +1,7 @@
 "use strict";
 
+const candidates = require("../component/CandidateList")
+
 function TaskController(router) {
 
     router.get("/ping", (ctx, next) => {
@@ -10,6 +12,19 @@ function TaskController(router) {
         throw new Error("fuck");
     });
 
+    /**
+     * 将音频优先进行压缩
+     *
+     * @param {Number} id 音频 ID
+     * @todo 需要对请求来源进行验证
+     */
+    router.post('/compress-sound', async (ctx, next) => {
+      let soundId = parseInt(ctx.request.body.id)
+      if (soundId <= 0) throw new Error('参数错误')
+      // 执行置顶操作
+      await candidates.topSound(soundId)
+      ctx.body = '音频已成功加入压缩队列队首'
+    })
     return router;
 }
 
