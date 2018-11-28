ae2453b0ff807c0b179b73fdfab783baf6f8953f
fishcg <353740902@qq.com>
2018-11-28 14:12:35 +0800

    使用 rpc 的方式获取请求 (#11)

--- middleware/index.js
+++ middleware/index.js
@@ -2,7 +2,11 @@
 
 const bodyParser = require("koa-bodyparser");
 const logger = require("koa-logger");
+const getRawBody = require('raw-body')
+const validator = require('validator');
 const { ENV } = require("../config/system");
+const { hash_hmac, base64_decode } = require("../lib/Utils")
+const { SECRET_KEY } = require("../config/key")
 
 function middleware(app) {
     app.use(async (ctx, next) => {
@@ -40,9 +44,49 @@ function middleware(app) {
         }
     });
 
+    // 处理 rpc 请求
+    app.use(async (ctx, next) => {
+        if (ctx.request.method === 'POST') {
+            let body = await getRawBody(ctx.req, { encoding: true });
+            body = parseBody(body);
+            if (!body) {
+                ctx.response.status = 403
+                throw new Error('无权访问');
+            }
+            ctx.request.body = body;
+        }
+        await next();
+    });
+
     app.use(bodyParser());
 
     return app;
+}
+
+/**
+ * 解析 rpc 请求中的 body
+ *
+ * @param {String} body 要解析的字符串，格式：<encodeData> <sign> <timestamp>
+ * @return {Object|null} 请求的参数
+ */
+function parseBody(body) {
+    if (body && typeof body === 'string') {
+        let parts = body.split(' ');
+        if (parts.length === 3 && validator.isNumeric(parts[2])) {
+            let rt = parseInt(parts[2]);
+            let now = Math.floor(Date.now() / 1000);
+            if (Math.abs(now - rt) < 100) {
+                let encodeData = parts[0];
+                let text = encodeData + ' ' + parts[2];
+                // 验证 sign
+                let hash = hash_hmac('sha256', text, SECRET_KEY);
+                if (hash === parts[1]) {
+                    return JSON.parse(base64_decode(encodeData));
+                }
+            }
+        }
+    }
+    return null;
 }
 
 module.exports = middleware;
